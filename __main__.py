dc914a793af7ac041e8ddc09c9e3f51662b5aed3
Robert DeForrest Reynolds <rldr.europa@gmail.com>
2026-01-26 09:29:38 -0800

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -26,13 +26,17 @@ async def Setup() -> None:
 
 
 
-@CrucibleBot.Bot.tree.command(name="challenge", description="Challenge another player to a fight")
+@CrucibleBot.Bot.tree.command(name="_challenge" if "Testing" in CrucibleBot.Name else "challenge", description="Challenge another player to a fight")
 async def challenge(Interaction:Interaction, challengee:Member, wager:float):
+	if CrucibleBot.Testing:
+		if not await CrucibleBot.Dev_Channel_Gate(Interaction): return
 	Challenge(Interaction, challengee, wager, C)
 
 
-@CrucibleBot.Bot.tree.command(name="arena", description="Invoke Main Event's Arena (Admin Only)")
+@CrucibleBot.Bot.tree.command(name="_arena" if "Testing" in CrucibleBot.Name else "arena", description="Invoke Crucible's Arena (Admin Only)")
 async def arena(Interaction:Interaction, action:str):
+	if CrucibleBot.Testing:
+		if not await CrucibleBot.Dev_Channel_Gate(Interaction): return
 	if Interaction.user.id not in CrucibleBot.Admins: return
 	if action == "begin":
 		await Interaction.response.send_message("Beginning arena tournament!", ephemeral=True)
@@ -40,8 +44,10 @@ async def arena(Interaction:Interaction, action:str):
 		await Interaction.response.send_message("Invalid action", ephemeral=True, delete_after=5)
 
 
-@CrucibleBot.Bot.tree.command(name="fighters", description="Manage your Main Event fighters")
+@CrucibleBot.Bot.tree.command(name="_fighters" if "Testing" in CrucibleBot.Name else "fighters", description="Manage your Crucible fighters")
 async def fighters(Interaction:Interaction):
+	if CrucibleBot.Testing:
+		if not await CrucibleBot.Dev_Channel_Gate(Interaction): return
 	Fighters(Interaction, C)
 
 
